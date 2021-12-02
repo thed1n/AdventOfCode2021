3addc19be01ce52429b346da92151d4ece4c69f4
thed1n <tomas.hedin@gmail.com>
2021-12-02 09:32:19 +0100

    day2

--- Day2/Day2.ps1
+++ Day2/Day2.ps1
@@ -1,1 +1,38 @@
-$data = get-content .\Day2\Input2.txt
+[string[]]$data = get-content .\Day2\Input2.txt
+
+[int32]$depth = 0
+[int32]$horizontal = 0
+[int32]$aim = 0
+[int32]$depth2 = 0
+
+$data| %{ 
+    
+    [string]$movement,[int32]$steps = $_ -split ' '
+
+    switch ($movement) {
+        'forward' {
+            $horizontal += $steps;
+             $depth2 += ($aim*$steps);
+             break
+            }
+        'up' {
+            $depth -= $steps;
+            $aim-= $steps;
+              break}
+        'down' {
+            $depth += $steps;
+            $aim+=$steps; 
+            break
+        }
+    }
+    if ($aim -lt 0) {$aim = 0}
+}
+
+
+
+
+
+[pscustomobject]@{
+    result1 = $depth * $horizontal
+    result2 = $depth2 * $horizontal
+}
