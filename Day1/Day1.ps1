13a872fa1298c9472ff55cd14cc53d2b96681d49
thed1n <tomas.hedin@gmail.com>
2021-12-05 15:40:17 +0100

    result fix

--- Day1/Day1.ps1
+++ Day1/Day1.ps1
@@ -1,1 +1,39 @@
-$data = get-content .\Day1\Input1.txt
+[CmdletBinding(SupportsShouldProcess)]
+
+[int[]]$data = get-content .\Day1\Input1.txt
+$increase = 0
+
+for ($i=1;$i -le $data.count;$i++) {
+
+    if ($data[$i-1] -lt $data[$i]) {
+        $increase++
+    }
+
+  
+    
+}
+$result = $increase
+
+
+$sliding = 0
+
+for ($i=0;$i -lt $data.count-3;$i+=1) {
+[int32]$val1 = 0
+[int32]$val2 = 0
+$val1 = $data[$i]+$data[$i+1]+$data[$i+2]
+$val2 = $data[$i+1]+$data[$i+2]+$data[$i+3]
+
+
+if ($val2 -gt $val1) {
+    $sliding++
+}
+
+write-verbose "[$i] [$val1] [$val2] [$sliding]"
+}
+
+$result2 = $sliding
+
+[PSCustomObject]@{
+   Part1 = $result
+   Part2 = $result2
+}
